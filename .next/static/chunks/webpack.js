8156ebe4a59b6315c99e02b90941cdc89119da26
Raja <raja.rathinam@pcisage.com>
2024-05-14 00:51:44 +0530

    init

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a992e45ace848884"; }
+/******/ 		__webpack_require__.h = function() { return "6cfbd1bd6f41a503"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/components/screens/home/sections/why_us/why_us.jsx
+++ src/components/screens/home/sections/why_us/why_us.jsx
@@ -5,12 +5,12 @@ import { Col, Image, Row } from "react-bootstrap";
 import styles from "./why_us.module.scss";
 
 const Card = ({ data }) => {
-  const { head, text, id } = data;
+  const { head, text, id ,img} = data;
 
   return (
     <Col xs={12} md={6} lg={3} className={styles.c}>
       <div className={styles.card}>
-        <Image src={`/assets/svg/${id}.svg`} alt={id} height={200} />
+        <Image src={`/assets/svg/${img}`} alt={id} height={200} />
         <h4>{head}</h4>
         <p>{text}</p>
       </div>
@@ -22,22 +22,26 @@ const WhyUs = () => {
   const cards = [
     {
       id: "comm",
+      img: "comm.svg",
       head: "0% Commission",
       text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam suscipit provident error?",
     },
     {
       id: "pickup",
+      img: "pickup.svg",
       head: "Pick-Up at Door",
       text: "We will pick you up from where you are and drop you exactly where you need to be.",
     },
     {
       id: "drop",
+      img: "drop.jpg",
       head: "Quick Drop",
       text: "Trend setters of smart commute be it traffic or rush hours.",
     },
 
     {
       id: "price2",
+      img: "price2.svg",
       head: "Affordable Price",
       text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam suscipit provident error?",
     },
