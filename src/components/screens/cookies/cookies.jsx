4fdc3238ecf2f4113fd7cded4f5391685987eeb4
raidotaxi <web.raidotaxi@gmail.com>
2024-07-11 13:05:42 +0530

    Update cookies.jsx

--- src/components/screens/cookies/cookies.jsx
+++ src/components/screens/cookies/cookies.jsx
@@ -4,10 +4,9 @@ import styles from './CookiesPolicy.module.scss';
 const CookiesPolicy = () => {
   return (
     <div className={styles.cookiesContainer}>
-    
-        <h1>Raido Cookies Policy</h1>
-        <p>This Cookies Policy explains how Raido we, us, our uses cookies and similar technologies on our website and mobile application. By using our services, you consent to the use of cookies in accordance with this policy.</p>
-</div>
+      <h1>Raido&apos;s Cookies Policy</h1>
+      <p>This Cookies Policy explains how Raido we, us, our uses cookies and similar technologies on our website and mobile application. By using our services, you consent to the use of cookies in accordance with this policy.</p>
+    </div>
   );
 };
 
