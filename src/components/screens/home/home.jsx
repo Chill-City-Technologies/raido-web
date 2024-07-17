d9ec4a41b149547e90cb7c0cbb0c9861c6a64d10
weuswebssohum <digitalmarketinguser7000rs@gmail.com>
2024-07-17 10:15:38 +0530

    Update home.jsx

--- src/components/screens/home/home.jsx
+++ src/components/screens/home/home.jsx
@@ -8,7 +8,12 @@ import WhyUs from "./sections/why_us/why_us";
 const HomeScreen = () => {
   return (
     <main>
-
+      <BannerSection />
+      <AboutSection/>
+      <WhyUs/>
+      <HowItWorksSection/>
+      <DownloadSection/>
+      
     </main>
   );
 };
