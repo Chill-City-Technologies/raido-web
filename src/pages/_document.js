548d38a5e091a0e29e05845a501f702991fb190e
midhun-developer <midhun.s.viswanathan@gmail.com>
2024-08-12 11:28:59 +0530

    updated icon link

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,8 +3,10 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <title>Raido</title>
+      <Head>
+        <link rel="icon" href="/favicon.svg" />
+        <title>Raido</title>
+      </Head>
       <body>
         <Main />
         <NextScript />
